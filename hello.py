b42f2eccba3654f993e991b943c628f906f79481
HiroakiFuse <hiroakifuse.hcs@gmail.com>
2018-10-19 22:10:48 +0900

    last

--- hello.py
+++ hello.py
@@ -4,20 +4,20 @@ from flask import Flask, jsonify,make_response
 
 # Flaskクラスのインスタンスを作成
 # __name__は現在のファイルのモジュール名
-api = Flask(__name__)
+app = Flask(__name__)
 
 # GETの実装
-@api.route('/get', methods=['GET'])
+@app.route('/get', methods=['GET'])
 def get():
     result = { "greeting": 'hello flask' }
     return make_response(jsonify(result))
 
 # エラーハンドリング
-@api.errorhandler(404)
+@app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 # ファイルをスクリプトとして実行した際に
 # ホスト0.0.0.0, ポート3001番でサーバーを起動
 if __name__ == '__main__':
-	api.run()
+	app.run()
